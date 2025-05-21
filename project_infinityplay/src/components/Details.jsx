709d42cb859bad3e3e96b66ec4cd789ba3aad57e
Luca Zuin <zuin.luca1111@gmail.com>
2025-05-21 13:01:41 +0200

    feat(pages): add HomePage and Details page components

---
+++ project_infinityplay/src/components/Details.jsx
@@ -0,0 +1,13 @@
+function Details() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <h1>Details</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Details;
---
+++ project_infinityplay/src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+function HomePage() {
+  return (
+    <Container className="text-light">
+      <h1>Benvenuto su InfinityPlay</h1>
+    </Container>
+  );
+}
+
+export default HomePage;
